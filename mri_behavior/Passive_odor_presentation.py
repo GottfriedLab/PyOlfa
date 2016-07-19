4b0044cc6ce21216e40f1dd61b5245eb2a28aad7
Jay Gottfried <jcsmelllab@gmail.com>
2016-07-19 17:41:59 -0500

    Update Passive_odor_presentation.py

--- mri_behavior/Passive_odor_presentation.py
+++ mri_behavior/Passive_odor_presentation.py
@@ -7,8 +7,6 @@ This protocol implements a passive odor paradigm for the Voyeur/Arduino
 platform. This includes the protocol behaviour as well as visualization (GUI).
 '''
 
-# TEST2
-
 # Python library imports
 from numpy import append, arange, hstack, nan, isnan, copy, negative
 from copy import deepcopy
